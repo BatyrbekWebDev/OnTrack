4a4b584e39dc26ea2b605aa83ce5842841112324
Batyrbek <43727077+BatyrbekWebDev@users.noreply.github.com>
2023-05-21 19:08:59 +0300

    Генерация данных временной шкалы

--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,17 +1,52 @@
+<script setup>
+import { generateTimelineItems } from '../functions'
+
+const timelineItems = generateTimelineItems()
+</script>
+
 <template>
   <div class="mt-7">
     <ul>
       <li
-        v-for="i in [0, 1, 2, 3, 4, 5]"
-        :key="i"
-        class="relative flex flex-col gap-2 border-t border-gray-200 px-4 py-10"
+        v-for="{ hour } in timelineItems"
+        :key="hour"
+        class="relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4"
       >
         <a
           href="#"
           class="absolute -top-4 left-1/2 -translate-x-1/2 rounded bg-gray-100 px-2 font-mono text-lg text-gray-500"
-          >{{ i }}:00</a
         >
+          {{ hour }}:00
+        </a>
       </li>
     </ul>
   </div>
 </template>
+<!-- import {
+    PAGE_TIMELINE,
+    PAGE_ACTIVITIES,
+    PAGE_PROGRESS,
+    HOURS_IN_DAY
+  } from './constants'
+  
+  export function normalizePageHash() {
+    const hash = window.location.hash.slice(1)
+  
+    if ([PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS].includes(hash)) {
+      return hash
+    }
+  
+    window.location.hash = PAGE_TIMELINE
+  
+    return PAGE_TIMELINE
+  }
+  
+  export function generateTimelineItems() {
+    const timelineItems = []
+  
+    for (let hour = 0; hour < HOURS_IN_DAY; hour++) {
+      timelineItems.push({ hour })
+    }
+  
+    return timelineItems
+  } -->
