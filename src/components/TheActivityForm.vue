1888cc2788bf315aa1f26db1edc99aaf7fe376f5
Batyrbek <43727077+BatyrbekWebDev@users.noreply.github.com>
2023-05-25 23:40:55 +0300

    Директива v-model

--- src/components/TheActivityForm.vue
+++ src/components/TheActivityForm.vue
@@ -1,29 +1,30 @@
 <script setup>
+import { ref } from 'vue'
 import BaseButton from './BaseButton.vue'
 import { isActivityValid } from '../validators'
 import { PlusIcon } from '@heroicons/vue/24/outline'
 
 const emit = defineEmits({
-  submit: isActivityValid,
+  submit: isActivityValid
 })
 
-let activity = 'New Task'
+const activity = ref('New Task')
 
 function submit() {
-  emit('submit', activity)
+  emit('submit', activity.value)
+  activity.value = ''
 }
 </script>
 
 <template>
   <form @submit.prevent="submit" class="sticky bottom-[57px] flex gap-2 border-t bg-white p-4">
     <input
-      :value="activity"
       type="text"
-      @input="activity = $event.target.value"
+      v-model="activity"
       class="w-full rounded border px-4 text-xl"
       placeholder="Activity name"
     />
-    <BaseButton>
+    <BaseButton :disabled="activity.trim() === ''">
       <PlusIcon class="h-8" />
     </BaseButton>
   </form>
