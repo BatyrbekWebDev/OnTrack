faacbfdf8bdf1c19f23089b13b8ee0fd5e9becc8
Batyrbek <43727077+BatyrbekWebDev@users.noreply.github.com>
2023-05-26 13:54:24 +0300

    Функция для генерации активностей

--- src/constants.js
+++ src/constants.js
@@ -13,6 +13,8 @@ export const BUTTON_TYPES = [
 export const PAGE_TIMELINE = 'timeline'
 export const PAGE_ACTIVITIES = 'activities'
 export const PAGE_PROGRESS = 'progress'
+
+export const SECONDS_IN_HOUR = 3600
 export const HOURS_IN_DAY = 24
 export const MIDNIGHT_HOUR = 0
 
--- src/functions.js
+++ src/functions.js
@@ -1,4 +1,4 @@
-import { PAGE_TIMELINE, HOURS_IN_DAY, MIDNIGHT_HOUR } from './constants'
+import { PAGE_TIMELINE, HOURS_IN_DAY, MIDNIGHT_HOUR, SECONDS_IN_HOUR } from './constants'
 import { isPageValid } from './validators'
 
 export function normalizePageHash() {
@@ -8,6 +8,30 @@ export function normalizePageHash() {
   }
   window.location.hash = PAGE_TIMELINE
   return PAGE_TIMELINE
+}
+
+export function generateActivities() {
+  return [
+    {
+      id: id(),
+      name: 'Coding',
+      secondsToComplete: 0 * SECONDS_IN_HOUR
+    },
+    {
+      id: id(),
+      name: 'Training',
+      secondsToComplete: 1 * SECONDS_IN_HOUR
+    },
+    {
+      id: id(),
+      name: 'Reading',
+      secondsToComplete: 2 * SECONDS_IN_HOUR
+    }
+  ]
+}
+
+export function id() {
+  return Date.now().toString(36) + Math.random().toString(36).substring(2)
 }
 
 export function generateTimelineItems() {
@@ -20,8 +44,8 @@ export function generateTimelineItems() {
 }
 
 export function generateActivitySelectOptions(activities) {
-  return activities.map((label, value) => ({
-    label,
-    value
+  return activities.map((activity) => ({
+    value: activity.id,
+    label: activity.name
   }))
 }
