f9df2b82f2196a5d3665563c4d89dff7664047d2
Batyrbek <43727077+BatyrbekWebDev@users.noreply.github.com>
2023-05-26 13:31:28 +0300

    Альтернативный компонент вместо пустого списка активностей

--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -2,6 +2,7 @@
 import { validateActivities, isActivityValid } from '../validators'
 import ActivityItem from '../components/ActivityItem.vue'
 import TheActivityForm from '../components/TheActivityForm.vue'
+import TheActivitiesEmptyState from '../components/TheActivitiesEmptyState.vue'
 defineProps({
   activities: {
     required: true,
@@ -17,8 +18,8 @@ const emit = defineEmits({
 
 </script>
 <template>
-  <div>
-    <ul class="divide-y">
+  <div class="flex flex-col grow">
+    <ul v-if="activities.length" class="divide-y grow">
       <ActivityItem
         v-for="activity in activities"
         :key="activity"
@@ -26,6 +27,7 @@ const emit = defineEmits({
         @delete="emit('deleteActivity', activity)"
       />
     </ul>
+    <TheActivitiesEmptyState v-else/>
     <TheActivityForm @submit="emit('createActivity', $event)"/>
   </div>
 </template>
