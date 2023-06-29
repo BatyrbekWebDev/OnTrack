2b06b8debc62e9b5dc9c05bbb53a1ffdcef0d4dc
Batyrbek <43727077+BatyrbekWebDev@users.noreply.github.com>
2023-06-29 20:24:45 +0300

    Выбор активности для каждого часа дня

--- src/constants.js
+++ src/constants.js
@@ -4,11 +4,7 @@ export const BUTTON_TYPE_PRIMARY = 'primary'
 export const BUTTON_TYPE_NEUTRAL = 'neutral'
 export const BUTTON_TYPE_DANGER = 'danger'
 
-export const BUTTON_TYPES = [
-  BUTTON_TYPE_PRIMARY,
-  BUTTON_TYPE_NEUTRAL,
-  BUTTON_TYPE_DANGER
-]
+export const BUTTON_TYPES = [BUTTON_TYPE_PRIMARY, BUTTON_TYPE_NEUTRAL, BUTTON_TYPE_DANGER]
 
 export const PAGE_TIMELINE = 'timeline'
 export const PAGE_ACTIVITIES = 'activities'
