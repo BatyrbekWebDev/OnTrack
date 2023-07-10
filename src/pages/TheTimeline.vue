2d739b59fbd8b6bd460600bab8476e7ce536805a
Batyrbek <43727077+BatyrbekWebDev@users.noreply.github.com>
2023-07-10 21:35:23 +0300

    Прокрутка к текущему часу

--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref, onMounted } from 'vue'
 import {
   validateTimelineItems,
   validateSelectOptions,
@@ -7,6 +8,7 @@ import {
   isActivityValid
 } from '../validators'
 import TimelineItem from '../components/TimelineItem.vue'
+import { MIDNIGHT_HOUR } from '../constants'
 
 defineProps({
   timelineItems: {
@@ -31,6 +33,21 @@ const emit = defineEmits({
     return [isTimelineItemValid(timelineItem), isActivityValid(activity)].every(Boolean)
   }
 })
+
+const timelineItemRefs = ref([])
+
+onMounted(scrollToCurrentTimelineItem)
+
+function scrollToCurrentTimelineItem() {
+  const currentHour = new Date().getHours()
+
+  if (currentHour === MIDNIGHT_HOUR) {
+    document.body.scrollIntoView()
+  } else {
+    timelineItemRefs.value[currentHour - 1].$el.scrollIntoView()
+  }
+  timelineItemRefs.value[currentHour - 1].$el.scrollIntoView()
+}
 </script>
 
 <template>
@@ -42,6 +59,7 @@ const emit = defineEmits({
         :timeline-item="timelineItem"
         :activities="activities"
         :activity-select-options="activitySelectOptions"
+        ref="timelineItemRefs"
         @selectActivity="emit('setTimelineItemActivity', timelineItem, $event)"
       />
     </ul>
